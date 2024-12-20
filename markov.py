2515d48952dba22bba8220021502ee909671d72b
Jan-Ole Stein <45128898+janolestein@users.noreply.github.com>
2024-12-20 18:30:00 +0100

    stat dist

--- markov.py
+++ markov.py
@@ -1,5 +1,6 @@
 import numpy as np
- 
+import scipy.linalg
+from numpy.linalg import matrix_power
 states = ["A", "B", "C"]
  
 transitionMatrix = np.array([[0.3, 0.6, 0.1], [0.2, 0.6, 0.2], [0.2,0.3,0.5]])
@@ -8,7 +9,28 @@ state = np.random.choice([0,1,2], p=startingDistribution)
  
 print(states[state], "->", end=" ")
  
-for i in range(20):
+for i in range(100):
     state = np.random.choice([0, 1, 2], p=transitionMatrix[state])
     print(states[state], "->", end=" ")
 print("end")
+
+transitionExp = transitionMatrix
+for i in range(4):
+    print(i)
+    transitionExp = np.matmul(transitionExp,transitionMatrix)
+
+print("Matrix: \n", transitionExp, "\n")
+
+nIterDist = np.matmul(startingDistribution, transitionExp)
+print(nIterDist)
+stable = np.matmul(startingDistribution, transitionExp)
+print("Stable: ", stable)
+randomStart = np.array([0.1, 0.9, 0.0]) 
+stable2 = np.matmul(randomStart, transitionExp)
+print("Stable: ", stable2)
+
+stableMatrix = startingDistribution
+for i in range(90):
+    stableMatrix = np.matmul(stableMatrix, transitionMatrix)
+
+print(stableMatrix)
